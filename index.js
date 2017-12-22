8459442b93d0ae98e45676ac7fdfe4a280bd7303
James Briggs <jfbriggs106@gmail.com>
2017-12-22 14:00:49 -0800

    Update index.js, add webpack config + public folder

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var path = require ('path');
 
 // Define web server port
 
 var port = process.env.PORT || 8008;
+
+// Serve static/client files from the client folder
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
 
 // ===== MIDDLEWARE =====
 
